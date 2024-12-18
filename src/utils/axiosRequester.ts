e20e590fdbc58620f1fd7d09120b095ddb99dbc9
Guilherme Maier <guilherme@enzo.com.br>
2024-12-18 01:45:40 -0300

    .

--- src/utils/axiosRequester.ts
+++ src/utils/axiosRequester.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 axios.interceptors.request.use((config) => {
   let configuration = config;
-  configuration.baseURL = "http://localhost:3000";
+  configuration.baseURL = "https://api.ipbcaxiasdosul.com.br";
   configuration.timeout = 300000;
   return configuration;
 });
