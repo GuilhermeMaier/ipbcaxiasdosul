c0cc2a7769ec3a655388c64ad6e14ea043a150df
Guilherme Maier <guilherme@enzo.com.br>
2024-12-20 23:44:38 -0300

    .

--- src/utils/axiosRequester.ts
+++ src/utils/axiosRequester.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 axios.interceptors.request.use((config) => {
   let configuration = config;
-  configuration.baseURL = process.env.NEXT_PUBLIC_API_URL;
+  configuration.baseURL = process.env.API_URL;
   configuration.timeout = 300000;
   return configuration;
 });
